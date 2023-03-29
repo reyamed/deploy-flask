1c56681c377dbf63db7aafe476b5f6b75d68b1c2
Ahmed AZIZ <ahmed.aziz@ump.ac.ma>
2023-03-30 01:18:23 +0200

    profile

---
+++ analyse.py
@@ -0,0 +1,112 @@
+from io import StringIO
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+import spacy
+
+nlp = spacy.load("fr_core_news_sm")
+def analysefunc(content):
+    #lire les données
+    data = pd.read_csv(StringIO(content))
+
+    #Crée Dataframe avec que la colonne burst
+    df_burst = pd.DataFrame().assign(burst=data["burst"])
+
+    #Suppresion données vides
+    df_burst = df_burst.dropna()
+
+    #Suppresion données sans caractères
+    df_burst = df_burst[df_burst['burst'].str.strip().astype(bool)]
+
+    #Création du modèle
+    documents = [TaggedDocument(words=doc.split(),tags=[i]) for i,doc in enumerate(df_burst['burst'])]
+    d2v_model = Doc2Vec(min_count=1)
+    d2v_model.build_vocab(documents)
+
+    vecteurs = np.vstack((d2v_model.dv[0],d2v_model.dv[1]))
+    i=2
+    while i<len(d2v_model.dv):
+        vecteurs = np.vstack((vecteurs,d2v_model.dv[i]))
+        i+=1
+
+    
+    df_vecteurs = pd.DataFrame(vecteurs, columns = ["vecteur" + str(i) for i in range(100)])
+
+    df_kmeans = data.dropna(subset = ['burst'])
+    df_kmeans = df_kmeans[df_kmeans['burst'].str.strip().astype(bool)]
+
+    df_kmeans['duree_pause'] = df_kmeans['duree_pause'].fillna(0)
+
+    i=0
+    while i < 100:
+        df_kmeans["vecteur"+str(i)]= vecteurs[:,i]
+        i+=1
+
+    df_kmeans = df_kmeans._get_numeric_data()
+
+    df_kmeans = df_kmeans.drop(['session','redacteur','debut_burst','longueur_burst','startPos','endPos','docLength'], axis=1)
+
+    df_kmeans = df_kmeans[df_kmeans['duree_pause'] >= 2.0]
+
+    distortions = []
+    K = range(1,10)
+    for k in K:
+        kmeanModel = KMeans(n_clusters=k)
+        kmeanModel.fit_predict(df_kmeans)
+        distortions.append(kmeanModel.inertia_)
+
+
+    km = KMeans(n_clusters=5)
+    df_kmeans["cluster"]=km.fit_predict(df_kmeans)
+    df_kmeans[df_kmeans.cluster==0].shape
+
+    df_cluster = data.dropna(subset = ['burst'])
+    df_cluster = df_cluster[df_cluster['burst'].str.strip().astype(bool)]
+    df_cluster['duree_pause'] = df_cluster['duree_pause'].fillna(0)
+    df_cluster = df_cluster[df_cluster['duree_pause'] >= 2.0]
+
+    df_cluster["cluster"]=df_kmeans["cluster"]
+
+    df_cluster[df_cluster.cluster==4].shape
+
+    df_cluster0 = df_cluster[df_cluster.cluster==0]
+    df_cluster1 = df_cluster[df_cluster.cluster==1]
+    df_cluster2 = df_cluster[df_cluster.cluster==2]
+    df_cluster3 = df_cluster[df_cluster.cluster==3]
+    df_cluster4 = df_cluster[df_cluster.cluster==4]
+# Create a list of dictionaries for each cluster with the specified format
+    clusters = []
+    for i in range(5):
+        df_cluster_i = df_cluster[df_cluster.cluster==i]
+        repeated = []
+        for j, b in enumerate(df_cluster_i["burst"]):
+            cluster_dict = {}
+            if b in repeated:
+                k = df_cluster_i[df_cluster_i["burst"] == b].index[repeated.count(b)]
+            else: 
+                k = df_cluster_i[df_cluster_i["burst"] == b].index[0]
+            cluster_dict["burstID"] = k
+            cluster_dict["clusterID"] = i
+            cluster_dict["burstLength"] = float(df_cluster_i.iloc[j]["longueur_burst"])
+            cluster_dict["pauseLength"] = float(df_cluster_i.iloc[j]["duree_pause"])
+            cluster_dict["textContent"] = postag(df_cluster_i.iloc[j]["burst"])
+            repeated.append(b)
+            clusters.append(cluster_dict)
+    sorted_clusters = sorted(clusters, key=lambda k: k['burstID'])
+
+    for i in range(len(sorted_clusters)):
+        sorted_clusters[i]['burstID'] = i+1
+    
+    return sorted_clusters
+
+
+def postag(text):
+    tex = nlp(text)
+    l = []
+    for token in tex:
+        new_string = token.text.replace("'", "")
+        l.append([new_string, token.pos_])
+    return l
---
+++ app.py
@@ -0,0 +1,372 @@
+import csv
+import datetime
+from io import StringIO
+from flask import Flask, request, jsonify, session, render_template
+import json
+from flask_mysqldb import MySQL
+# utiliser mysql avec flask
+from flask_cors import CORS, cross_origin
+import os
+import uuid 
+from werkzeug.utils import secure_filename
+import pyrebase
+from flask_session import Session
+#import pyrebase
+import urllib.request
+import pandas as pd
+from analyse import analysefunc
+# local uploads 
+UPLOAD_FOLDER = "./uploads"
+ALLOWED_EXTENTIONS = {"csv", "png", "jpeg", "gif"}
+
+
+#filter mime-types 
+def allowed_files(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENTIONS
+
+app = Flask(__name__, static_folder='static')
+app.secret_key = 'your_secret_key'
+#config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config["MYSQL_DB"] = "visualisation"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['SESSION_COOKIE_SAMESITE'] = "None"
+Session(app)
+
+# app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
+# session['loggedin'] = False
+# session['id'] = ""
+# session['email'] = ""
+# session['firstname'] = ""
+# session['lastname'] = ""
+# session['password'] = ""
+mysql = MySQL(app)
+cors = CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True}})
+
+#fire base config
+config = {
+    "apiKey": "AIzaSyBAghCslXj0tiegqo-_BJzOeaLZH_DvpSY",
+  "authDomain": "visualisation-c78b5.firebaseapp.com",
+  "projectId": "visualisation-c78b5",
+  "storageBucket": "visualisation-c78b5.appspot.com",
+  "messagingSenderId": "584789840609",
+  "appId": "1:584789840609:web:04e2cabef94a4a8c365da0",
+  "serviceAccount": "./keyfile.json",
+  "databaseURL": "https://visualisation-c78b5-default-rtdb.firebaseio.com"
+
+}
+
+#init firebase app
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+#real time database instance
+db = firebase.database()
+#storage
+storage = firebase.storage()
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route("/api/posts", methods=["GET"])
+# def index():
+#     if request.method == "GET":
+#         return jsonify(data="posts main response")
+
+# @app.route("/api/addposts", methods=["POST"])
+#     if request.method == "POST":
+        
+@app.route("/api/register", methods=["POST"])
+def register():
+    if request.method == "POST":
+
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        try:
+            # email = "ahfmeereizi@gmail.com"
+            # password = "11111tbrtbrtb"
+        # Where I added my shit
+        #create the user
+            user = auth.create_user_with_email_and_password(email, password)
+            # #login the user right away
+            # user = auth.sign_in_with_email_and_password(email, password)   
+            userinfo = {
+                "email":email,
+                "password":password,
+                "firstname":firstname,
+                "lastname":lastname
+            }
+            local = auth.get_account_info(user['idToken'])
+            localId = local['users'][0]['localId']
+           
+            # # Wher I ended my shit
+            # # json_data = request.get_json()
+            # # print(json_data)
+            # # firstname = json_data['firstname']
+            # # lastname = json_data['lastname']
+            # # email=json_data['email']
+            # # password=json_data['password']
+
+            # # Save user data in Firebase Realtime Database
+            db.child('users').child(localId).set(userinfo)
+            status = "success"
+        except:
+            status = "email already exists"
+            #get cursor to exec the mysql functions
+        # try:
+        #     cur = mysql.connection.cursor()
+        #     cur.execute(""" INSERT INTO user (firstname, lastname, email, password) VALUES (%s, %s, %s, %s)  """,
+        #         (firstname, lastname, email, password)
+        #     )
+        #     mysql.connection.commit()
+        #     cur.close()
+        #     status = "success"
+        # except: 
+        #     status = 'this user is already registered'
+    return jsonify({'result': status})
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST':
+        # Create variables for easy access
+
+        # json_data = request.get_json()
+        # print(json_data)
+        # firstname = json_data['firstname']
+        # lastname = json_data['lastname']
+        # email=json_data['email']
+        # password=json_data['password']
+        email = request.form['email']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        try:
+        # Verify user credentials with Firebase Authentication
+            # email = "ahfmeereizi@gmail.com"
+            # password = "11111tbrtbrtb"
+            user = auth.sign_in_with_email_and_password(email, password)
+            print("AAA")
+            local = auth.get_account_info(user['idToken'])
+            localId = local['users'][0]['localId']
+            print(localId)
+            orderedDict = db.child("users").order_by_key().equal_to(localId).limit_to_first(1).get()
+            items = list(orderedDict.val().items())
+            print(items[0][1]['email'])
+            # user_data = users_ref.child(localId).get()
+            # print(user_data)
+            # session['user'] = user.uid
+            session['loggedin'] = True
+            session['id'] = items[0][0]
+            session['email'] = items[0][1]['email']
+            session['firstname'] = items[0][1]['firstname']
+            session['lastname'] = items[0][1]['lastname']
+            status = "success"
+            return jsonify({'status': True})
+        except:
+            print("error")
+            status = "shit"
+            return jsonify({'status': False})
+
+        # cursor = mysql.connection.cursor()
+        # cursor.execute('SELECT * FROM user WHERE email = %s AND password = %s', (email, password,))
+        # # Fetch one record and return result
+        # account = cursor.fetchone()
+        # print(type(account))
+        # print(account)
+        # # If account exists in accounts table in out database
+        # if account:
+        #     # Create session data, we can access this data in other routes
+        #     session['loggedin'] = True
+        #     session['id'] = account[0]
+        #     session['email'] = account[3]
+        #     session['firstname'] = account[1]
+        #     session['lastname'] = account[2]
+        #     session['password'] = account[4]
+        #     print(session)
+        #     return jsonify({'status': True})
+        # else:
+        #     # Account doesnt exist or username/password incorrect
+        #     msg = 'Incorrect email/password!'
+        #     return jsonify({'status': False})
+        
+@app.route('/api/getuser',  methods=['POST'])
+def getuser():
+    if request.method == "POST":
+        print(session)
+        if session.get("loggedin"):
+            # return jsonify({'status': "heeof"})
+            return jsonify({"id": session['id'], "email": session["email"], "firstname": session['firstname'], "lastname": session['lastname']})
+        else:
+            return None
+
+@app.route('/api/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+
+   return jsonify({'status': "logged out"})
+
+
+@app.route("/api/addpost", methods=["POST"])
+def addpost():
+    if request.method == "POST":
+        # print(request.form, flush=True)
+
+        # title = request.form.get("title")
+        # content = request.form.get("content")
+        cover = request.files["cover"]
+        if cover.filename.split(".")[1] == "csv":
+            filename = str(uuid.uuid4())
+            filename += "."
+            filename += cover.filename.split(".")[1]
+
+            #save the file inside the uploads folder
+            cover.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+            #local file
+            local_filename = "./uploads/"
+            local_filename += filename   
+
+            #firebase filename
+            firebase_filename = "uploads/"
+            firebase_filename += filename
+
+            #upload the file
+            storage.child(firebase_filename).put(local_filename)
+            #get the url of the file
+            cover_image = storage.child(firebase_filename).get_url(None)
+            infofile = {
+                "filename": filename,
+                "URL": cover_image
+            }
+            print("le fichier ajouté", filename, cover_image)
+            #get cursor to exec the mysql functions
+
+            print(session['firstname'])
+            try:
+                
+                localId = session["id"]
+                db.child("users").child(localId).child("csvfile").push(infofile)
+                print("gggggggggggggggggggggggggggggggg")
+                # orderedDict = db.child("users").order_by_key().equal_to(localId).limit_to_first(1).get()
+
+            # cur = mysql.connection.cursor()
+            # cur.execute(""" INSERT INTO fichier ( cover, covername) VALUES (%s, %s)  """, (cover_image, filename))
+            # mysql.connection.commit()
+            # cur.close()
+                status = filename
+
+                
+                
+            except: 
+                print("ffffffffffffffffffffff")
+                status = 'fail'
+                
+
+            # os.remove(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+            return jsonify({"data" : status})
+        else:
+
+            return jsonify({"data" : "fail"})
+
+@app.route('/api/protected',  methods=['GET'])
+def protected():
+    if 'loggedin' in session:
+        if session['loggedin']:
+            return jsonify({"authenticated" : True})
+        else:
+            return jsonify({"authenticated" : False})
+    else:
+        return jsonify({"authenticated" : False})
+
+
+
+
+@app.route("/api/analyse", methods=["POST"])   
+def analyse():
+    if request.method == "POST":
+        print(request.form)
+        data1 = request.get_json()
+        filename = data1.get('filename')
+        filename = 'uploads/' + filename
+        print(filename)
+        print(type(filename))
+        blob = storage.bucket.blob(filename)
+        url = blob.generate_signed_url(
+            expiration=datetime.timedelta(minutes=15),
+            method="GET"
+        )
+
+        if filename.split(".")[1] == "csv":
+        # Retrieve contents of uploaded file from Firebase Storage
+            with urllib.request.urlopen(url) as url:
+                content = url.read().decode("latin1")
+
+            # csv_data = pd.read_csv(StringIO(content))
+
+        # Do something with CSV data...
+        # For example, print out the first 5 rows of the CSV file
+            # print(csv_data.head())
+            print('avant')
+            result = analysefunc(content)
+            print('apres')
+            #local filename
+            filenamejson = filename.split(".")[0]
+            filenamejson += ".json"
+
+
+            #firebase filename
+            firebase_filename = filenamejson
+            with open(filenamejson, "w") as f:
+                jsonify(json.dump(result, f))
+
+            storage.child(firebase_filename).put(filenamejson)
+            print("fichier enregistré")
+            #get the url of the file
+            cover_image = storage.child(firebase_filename).get_url(None)
+           
+            infofile = {
+                "filename": filenamejson,
+                "URL": cover_image
+            }
+            try:
+                if "id" in session:
+                    localId = session["id"]
+                    db.child("users").child(localId).child('jsonfiles').push(infofile)
+                    
+                # cur = mysql.connection.cursor()
+                # cur.execute(""" INSERT INTO fichier ( cover, covername) VALUES (%s, %s)  """, (cover_image, filename))
+                # mysql.connection.commit()
+                # cur.close()
+                status = filenamejson
+            except: 
+                status = 'success'
+        else:
+            status = "nothing"
+        return jsonify({"json" : status})
+    else:
+        return {{"json" : "nothing"}}
+    
+
+
+#recuper fichier
+#analyser
+#stocker resultat dans un fichier json
+#envoyer fichier json au front
+
+
+   # Redirect to login page
+if __name__== "__main__":
+    app.run()
+
+
